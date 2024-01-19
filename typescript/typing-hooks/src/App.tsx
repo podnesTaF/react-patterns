c5d05a7e180bc614d755a1e8d9e102f11731f1cd
podnesTaF <podnestrade@gmail.com>
2024-01-19 22:43:24 +0100

    typinh

--- typescript/typing-hooks/src/App.tsx
+++ typescript/typing-hooks/src/App.tsx
@@ -1,11 +1,31 @@
-import ShoppingCard from "./components/shopping-card";
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
+import Book from "./components/books/book";
+import Books from "./components/books/books";
+
+export type IBook = {
+  id: number;
+  title: string;
+  author?: string;
+};
+
+export const fetchBooks = async (count: number) => {
+  const response = await axios.get(`/api/books?limit=${count}`);
+  return response.data;
+};
 
 function App() {
+  const [books, setBooks] = useState<IBook[]>([]);
+
   return (
-    <main className="flex items-center h-full place-content-center bg-cyan-900">
-    <ShoppingCard />
-  </main>
+    <main className="w-full max-w-2xl py-16 mx-auto">
+      <Books setBooks={setBooks}>
+        {books.map((book) => {
+          return <Book title={book.title} author={book.author} />;
+        })}
+      </Books>
+    </main>
   );
 }
 
